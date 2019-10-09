8f5c2b1658e9cbae72e17c7536d21a42f7a2adfe
Elisa Kamir <elisa.kamir@outlook.fr>
2019-10-09 09:56:58 +0200

    hebdo

--- boot_test.R
+++ boot_test.R
@@ -1,20 +1,84 @@
-library(s2dverification)
-
 tictoc::tic()
-testy <- Ano_CrossValid(array_prev, array_clim)
-tictoc::toc()
+#Spearman <- s2dverification:::.Corr
+acc_prev <- vector()
+acc_clim <- vector()
+msess <- vector()
 
-prev_anomaly <- array(NA,dim=c(ngrids,nan,nrun,ndimT))
-clim_anomaly <- array(NA,dim=c(ngrids,nan,nrun,ndimT))
-obs_anomaly <- array(NA,dim=c(ngrids,nan,ndimT))
+corel <- function(x,y){
+  out <- veriApply("Spearman", x, y, method = 'spearman')$corr
+  return(out)
+}
 
-for (y in 1:nan){
-  cv_mean_prev <- apply(array_prev[,-y,,], c(1,3,4), mean)
-  cv_mean_clim <- apply(array_clim[,-y,,], c(1,3,4), mean)
-  cv_mean_obs <- apply(array_obs[,-y,], c(1,3), mean)
+for (ind in 3:5){
 
-  prev_anomaly[,y,,] <- array_prev[,y,,]-cv_mean_prev
-  clim_anomaly[,y,,] <- array_clim[,y,,]-cv_mean_clim
-  obs_anomaly[,y,] <- array_obs[,y,]-cv_mean_obs
+  # getDoParWorkers()
+  # getDoParName()
+  # registerDoSNOW(makeCluster(2, type = "SOCK"))
+  # getDoParWorkers()
+  # getDoParName()
+  # , .verbose = T
+  cl <- makeCluster(no_core)
+  registerDoParallel(cl)
+
+  foreach(b=1:8, .packages=c('easyVerification', 's2dverification'), .export = "corel") %dopar%  { # loop on bootstrap samples
+
+    #Spearman <- s2dverification:::.Corr
+
+    nom_ref <- paste0(indic[ind],b,"_ref.RData")
+    nom_prev <- paste0(indic[ind],b,"_prev.RData")
+    nom_clim <- paste0(indic[ind],b,"_clim.RData")
+    nom_an_prev <- paste0(indic[ind],b,"_anomaly_prev.RData")
+    nom_an_clim <- paste0(indic[ind],b,"_anomaly_clim.RData")
+    nom_an_obs <- paste0(indic[ind],b,"_anomaly_obs.RData")
+
+    load(file = file.path(dir_data, "extract", nom_ref))
+    load(file = file.path(dir_data, "extract", nom_prev))
+    load(file = file.path(dir_data, "extract", nom_clim))
+    load(file = file.path(dir_data, "extract", nom_an_prev))
+    load(file = file.path(dir_data, "extract", nom_an_clim))
+    load(file = file.path(dir_data, "extract", nom_an_obs))
+
+    # ensemble mean
+    prev_mean <- apply(array_prev, c(1,2,4), mean)
+    clim_mean <- apply(array_clim, c(1,2,4), mean)
+
+    ### Score computing: We compute bias, and cross validated rmse and correlation coefficient.
+
+    ### MONTHLY ###
+
+    det_month <- array(NA, dim = c(ngrids,3,ndimT))
+
+    for (t in 1:ndimT){
+
+      # MSESS
+
+      msess <- veriApply("EnsMsess", fcst=t(prev_mean[,,t]), fcst.ref=t(clim_mean[,,t]), obs=t(array_obs[,,t]))
+
+      # ACC #
+
+      # Pearson for continued indicators
+      if (ind!=3 & ind!=4 & ind!=5) {
+        # ref vs prev
+        acc_prev <- veriApply("EnsCorr", fcst=prev_anomaly[,,,t], obs=obs_anomaly[,,t])
+        # ref vs clim
+        acc_clim <- veriApply("EnsCorr", fcst=clim_anomaly[,,,t], obs=obs_anomaly[,,t])
+
+      } else { # Spearman for ordinal indicators
+        # ref vs prev
+        acc_prev <- veriApply(".Corr", fcst=prev_anomaly[,,,t], obs=obs_anomaly[,,t])
+        # ref vs clim
+        #acc_clim <- corel(clim_anomaly[,,,t], obs_anomaly[,,t])
+
+      }
+
+      #det_month[,,t] <- array(c(msess, acc_prev, acc_clim), dim=c(ngrids,3))
+
+    }
+
+
+  }
+  stopCluster(cl)
 
 }
+tictoc::toc()
+
