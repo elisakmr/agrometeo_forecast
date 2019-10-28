8dd7179153978d51409a40a8d794e5d8d0015a72
Elisa Kamir <elisa.kamir@outlook.fr>
2019-10-28 17:33:51 +0100

    managing na

--- boot_test.R
+++ boot_test.R
@@ -1,84 +1,61 @@
-tictoc::tic()
-#Spearman <- s2dverification:::.Corr
-acc_prev <- vector()
-acc_clim <- vector()
-msess <- vector()
+boot_month <- list()
+boot_seas <- list()
 
-corel <- function(x,y){
-  out <- veriApply("Spearman", x, y, method = 'spearman')$corr
-  return(out)
-}
+for(b in 1:nboot)   {
+  # loading gridded scores
 
-for (ind in 3:5){
+  # seasonal
+  nom_prevseas <- paste0(indic[ind], b,"scoredetseas_grid_prev.RData")
+  load(file = file.path(dir_data, "score", nom_prevseas))
 
-  # getDoParWorkers()
-  # getDoParName()
-  # registerDoSNOW(makeCluster(2, type = "SOCK"))
-  # getDoParWorkers()
-  # getDoParName()
-  # , .verbose = T
-  cl <- makeCluster(no_core)
-  registerDoParallel(cl)
+  box_prev_month <- matrix(nrow = ndimT, ncol = 3)
 
-  foreach(b=1:8, .packages=c('easyVerification', 's2dverification'), .export = "corel") %dopar%  { # loop on bootstrap samples
-
-    #Spearman <- s2dverification:::.Corr
-
-    nom_ref <- paste0(indic[ind],b,"_ref.RData")
-    nom_prev <- paste0(indic[ind],b,"_prev.RData")
-    nom_clim <- paste0(indic[ind],b,"_clim.RData")
-    nom_an_prev <- paste0(indic[ind],b,"_anomaly_prev.RData")
-    nom_an_clim <- paste0(indic[ind],b,"_anomaly_clim.RData")
-    nom_an_obs <- paste0(indic[ind],b,"_anomaly_obs.RData")
-
-    load(file = file.path(dir_data, "extract", nom_ref))
-    load(file = file.path(dir_data, "extract", nom_prev))
-    load(file = file.path(dir_data, "extract", nom_clim))
-    load(file = file.path(dir_data, "extract", nom_an_prev))
-    load(file = file.path(dir_data, "extract", nom_an_clim))
-    load(file = file.path(dir_data, "extract", nom_an_obs))
-
-    # ensemble mean
-    prev_mean <- apply(array_prev, c(1,2,4), mean)
-    clim_mean <- apply(array_clim, c(1,2,4), mean)
-
-    ### Score computing: We compute bias, and cross validated rmse and correlation coefficient.
-
-    ### MONTHLY ###
-
-    det_month <- array(NA, dim = c(ngrids,3,ndimT))
+  # monthly
+  if (ind!=3 & ind!=4 & ind!=5) {
+    nom_prevmonth <- paste0(indic[ind], b,"scoredetmonth_grid_prev.RData")
+    load(file = file.path(dir_data, "score", nom_prevmonth))
 
     for (t in 1:ndimT){
+      # box aggregation with latitude weighting
+      val_prevmonth <- list()
 
-      # MSESS
-
-      msess <- veriApply("EnsMsess", fcst=t(prev_mean[,,t]), fcst.ref=t(clim_mean[,,t]), obs=t(array_obs[,,t]))
-
-      # ACC #
-
-      # Pearson for continued indicators
-      if (ind!=3 & ind!=4 & ind!=5) {
-        # ref vs prev
-        acc_prev <- veriApply("EnsCorr", fcst=prev_anomaly[,,,t], obs=obs_anomaly[,,t])
-        # ref vs clim
-        acc_clim <- veriApply("EnsCorr", fcst=clim_anomaly[,,,t], obs=obs_anomaly[,,t])
-
-      } else { # Spearman for ordinal indicators
-        # ref vs prev
-        acc_prev <- veriApply(".Corr", fcst=prev_anomaly[,,,t], obs=obs_anomaly[,,t])
-        # ref vs clim
-        #acc_clim <- corel(clim_anomaly[,,,t], obs_anomaly[,,t])
+      for (i in 1:ngrids){
+        val_prevmonth[[i]] <- det_month[i,,t]*cos(pi*lati[i,1]/180)
+        coeff[i] <- cos(pi*lati[i,1]/180)
+        # removing NAs
+        if (length(which(is.na(det_month[i,])))>0){
+          coeff[i] <- NA
+          val_prevmonth[[i]] <- rep(NA,3)
+        }
 
       }
 
-      #det_month[,,t] <- array(c(msess, acc_prev, acc_clim), dim=c(ngrids,3))
+      box_prev_month[t,] <- apply(array(unlist(val_prevmonth) , c(3,ngrids)), 1, sum, na.rm = T)/sum(coeff, na.rm = T)
+      #box_prev_month[t,] <- Reduce("+", val_prevmonth)/sum(coeff)
 
     }
-
-
+    boot_month[[b]] <- box_prev_month
   }
-  stopCluster(cl)
+  # seasonal
+  val_prevseas <- list()
 
+  for (i in 1:ngrids){
+    val_prevseas[[i]] <- det_seas[i,]*cos(pi*lati[i,1]/180)
+    coeff[i] <- cos(pi*lati[i,1]/180)
+    # removing NAs
+    if (length(which(is.na(det_seas[i,])))>0){
+      coeff[i] <- NA
+      val_prevseas[[i]] <- rep(NA,3)
+    }
+  }
+
+  boot_seas[[b]] <- apply(array(unlist(val_prevseas) , c(3,ngrids)), 1, sum, na.rm = T)/sum(coeff, na.rm = T)
 }
-tictoc::toc()
+
+if (ind!=3 & ind!=4 & ind!=5) {
+
+  month_ps <- apply(array(unlist(boot_month) , c(6,3,nboot)), c(1,2), mean)
+}
+seas_ps <- apply(array(unlist(boot_seas) , c(3,nboot)), 1, mean, na.rm=T)
+
 
